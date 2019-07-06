e736e65aeaa02378315eb5d49b68adb3a7f57c5a
Minhajul Arafin Russel <rredxx@icloud.com>
2019-07-06 22:50:25 +0600

    developer account added, locationManager added, radar tracking is on

--- jac475/AppDelegate.swift
+++ jac475/AppDelegate.swift
@@ -6,19 +6,93 @@
 //
 
 import UIKit
+import CoreLocation
 import RadarSDK
 import Pilgrim
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate, RadarDelegate {
 
     var window: UIWindow?
+    var locationManager: CLLocationManager!
+    var seenError : Bool = false
+    var locationFixAchieved : Bool = false
+    var locationStatus : NSString = "Not Started"
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //Mr Jamal Cole //prj_test_pk_81c06ab3fa9fa0d2656ad26a179eff38b2eb5a6c
+        initLocationManager()
         Radar.initialize(publishableKey: "prj_test_pk_81c06ab3fa9fa0d2656ad26a179eff38b2eb5a6c")
+        Radar.setDelegate(self)
         return true
+    }
+    
+    // Location Manager helper stuff
+    func initLocationManager() {
+        seenError = false
+        locationFixAchieved = false
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    // Location Manager Delegate stuff
+    // If failed
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        if ((error) != nil) {
+            if (seenError == false) {
+                seenError = true
+                print("error is: \(error)")
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if (locationFixAchieved == false) {
+            locationFixAchieved = true
+            let locationArray = locations as NSArray
+            let locationObj = locationArray.lastObject as! CLLocation
+            let coord = locationObj.coordinate
+            
+            print("latitude: \(coord.latitude)")
+            print("longitude: \(coord.longitude)")
+        }
+    }
+    
+    // authorization status
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+
+        var shouldIAllow = false
+        
+        switch status {
+        case CLAuthorizationStatus.restricted:
+            locationStatus = "Restricted Access to location"
+        case CLAuthorizationStatus.denied:
+            locationStatus = "User denied access to location"
+        case CLAuthorizationStatus.notDetermined:
+            locationStatus = "Status not determined"
+        default:
+            locationStatus = "Allowed to location Access"
+            shouldIAllow = true
+        }
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "LabelHasbeenUpdated"), object: nil)
+        if (shouldIAllow == true) {
+            // Start location services
+            print("Location to Allowed")
+            locationManager.startUpdatingLocation()
+        } else {
+            print("Denied access: \(locationStatus)")
+        }
+    }
+
+    func didReceiveEvents(_ events: [RadarEvent], user: RadarUser) {
+        print("myBackgroundEvents:\(events)")
+        print("myUserData: \(user)")
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- jac475/StateVC.swift
+++ jac475/StateVC.swift
@@ -14,17 +14,14 @@ class StateVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        Radar.trackOnce(completionHandler: { (status: RadarStatus, location: CLLocation?, events: [RadarEvent]?, user: RadarUser?) in
+            // do something with status, location, events, user
+            print("radarStatus: \(status)")
+            print("radarLocation: \(location)")
+            print("radarEvents: \(events)")
+            print("radarUser: \(user?.place)")
+        })
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
