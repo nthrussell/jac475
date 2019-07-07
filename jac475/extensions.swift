e3bfad9e11f0f3ac445995ca182df8351980fa76
Minhajul Arafin Russel <rredxx@icloud.com>
2019-07-07 13:23:42 +0600

    notification name extension added

---
+++ jac475/extensions.swift
@@ -0,0 +1,14 @@
+//
+//  extensions.swift
+//  jac475
+//
+//  Created by russell on 7/7/19.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let RDbackgroundLocationUpdate = Notification.Name("RDbackgroundLocationUpdate")
+    static let RDBackgroundEvents         = Notification.Name("RDBackgroundEvents")
+    static let RDBackgroundUserPlace      = Notification.Name("RDBackgroundUserPlace")
+}
