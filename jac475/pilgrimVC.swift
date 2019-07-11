6c026a4af3d20d1054d98124b430a90d985ca58e
Minhajul Arafin Russel <rredxx@icloud.com>
2019-07-11 22:13:28 +0600

    pilgrim added

---
+++ jac475/pilgrimVC.swift
@@ -0,0 +1,20 @@
+//
+//  pilgrimVC.swift
+//  jac475
+//
+//  Created by russell on 11/7/19.
+//
+
+import UIKit
+import Pilgrim
+
+class pilgrimVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
