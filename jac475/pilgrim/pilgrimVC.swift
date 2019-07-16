0904d2ef29718341e3639855e0f7652480ed78e0
Minhajul Arafin Russel <rredxx@icloud.com>
2019-07-16 23:34:44 +0600

    pilgrimCell added

--- jac475/pilgrim/pilgrimVC.swift
+++ jac475/pilgrim/pilgrimVC.swift
@@ -8,21 +8,26 @@
 import UIKit
 import Pilgrim
 
-class pilgrimVC: UIViewController {
+class pilgrimVC: UIViewController, UITableViewDataSource, UITableViewDelegate, PilgrimManagerDelegate {
     
     @IBOutlet var pilgrimDescription: UITextView!
     @IBOutlet var tableView: UITableView!
+    var pilgrimPlaces = [String]()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        pilgrim()
+        
         //Background events
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(pilgrimVenuName(_:)),
                                                name:.pilgrimVenuName,
                                                object: nil)
-        
-        pilgrim()
         
     }
     
@@ -33,11 +38,31 @@ class pilgrimVC: UIViewController {
                 self.pilgrimDescription.text = "\(String(describing: currentLocation))"
             }
             print("FQLocationError: \(String(describing: error))")
-
+            
         }
         
-       _ = PilgrimManager.shared().triggerDescriptions
+    }
+    
+    func pilgrimManager(_ pilgrimManager: PilgrimManager, handle visit: Visit) {
+       let placeVisited =  "\(visit.hasDeparted ? "Departure from" : "Arrival at") \(visit.venue != nil ? visit.venue!.name : "Unknown venue."). Added a Pilgrim visit at: \(visit.displayName)"
+        self.pilgrimPlaces.append(placeVisited)
+        self.tableView.reloadData()
+        print("pilgrimPlaces \(pilgrimPlaces)")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pilgrimPlaces.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pilgrimCell") as! pilgrimCell
         
+        let data = self.pilgrimPlaces[indexPath.row]
+        print("pilgrimPlaces: \(data)")
+        cell.textLabel?.text = data
+        //cell.detailTextLabel?.text = data.descriptionOfPlace
+        
+        return cell
     }
     
     @objc func pilgrimVenuName(_ notification: Notification) {
